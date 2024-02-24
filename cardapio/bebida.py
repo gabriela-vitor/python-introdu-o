635906171c04f4cbc0fd602a5e6e9be074f196af
Gabriela <127972306+gabriela-vitor@users.noreply.github.com>
2024-02-24 19:52:04 -0300

    Add files via upload

---
+++ cardapio/bebida.py
@@ -0,0 +1,14 @@
+from modelos.cardapio.item_cardapio import ItemCardapio
+
+class Bebida(ItemCardapio):
+    def __init__(self, nome, preco, tamanho):
+        #acessar informacoes de outro local, no caso ItemCardapio
+        super().__init__(nome, preco)
+        self.tamanho = tamanho
+
+    def __str__(self):
+        return self._nome
+    
+    def aplicar_desconto(self):
+        self._preco -= (self._preco *0.20)
+        
